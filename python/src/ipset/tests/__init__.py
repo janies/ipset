2beceeb4cc2e9bd1afa59720554976ab550143c2
Douglas Creager <douglas.creager@redjack.com>
2010-03-02 18:24:40 -0500

    Low-level Python interface
    This patch adds Python bindings to the ipset library.  Right now,
    there's just a simple wrapper exposing all of the C functions, using
    the standard ctypes library.  The next step will be to make an
    object-oriented interface, which should be easier to use from other
    Python code.

---
+++ python/src/ipset/tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# Copyright © 2010, RedJack, LLC.
+# All rights reserved.
+#
+# Please see the LICENSE.txt file in this distribution for license
+# details.
+# ----------------------------------------------------------------------
